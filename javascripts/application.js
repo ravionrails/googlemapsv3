28290a8a08a4ddf3c33be587dafcd6f3506f4533
ravindra <ravindra@ravindra-desktop.(none)>
2010-11-13 01:10:42 +0530

    autocompletion added to search box , search function added (in started phase)

--- javascripts/application.js
+++ javascripts/application.js
@@ -10,12 +10,17 @@ jQuery(document).ready(function(){
 		}
 		
 	});
+
 	jQuery("#search_div > input[type=text]").blur(function(){
 		if( jQuery(this).val() == "" ){
 			jQuery(this).val('Enter your search place in Uttar Pradesh');
 			jQuery(this).css({'color': 'grey', 'font-family':'Cursive'});
 		}
 		
+	});
+
+	jQuery("#search_div > input[type=submit]").click(function(){
+		search();	
 	});
 
 	jQuery("#input_from, #input_to").click(function(){
@@ -56,6 +61,27 @@ jQuery(document).ready(function(){
 	initialize();
 	//setTimeout(5000, function(){jQuery('#map_canvas').css('position','absolute');});
 });
+
+function search(){
+    var police_latlng = new google.maps.LatLng(26.444344,83.532246);
+    var myOptions = {
+      zoom: 19,
+      center: police_latlng,
+      mapTypeId: google.maps.MapTypeId.ROAD ,
+      disableDefaultUI: true,
+
+    };
+    	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+	
+	var police = 'images/icon/police.png';
+	var police_marker = new google.maps.Marker({
+          position: police_latlng,
+          map: map,
+	  icon: police,
+          title:"Police Station"
+	});
+}
 
 function initialize() {
     var latlng = new google.maps.LatLng(26.7422,83.8902);
